2c07b8d1492a07ac505763baefaa89b3564f6237
SkipToMyLuLu <skip2mylulu@icloud.com>
2026-03-28 16:44:47 -0400

    testing to see if this works in an actual broswer

---
+++ script.js
@@ -0,0 +1,5 @@
+fetch('./words.json')
+    .then(response => response.json) //What im guessing this does: probably takes the output of fetch
+    // and the .then turns response into that output, and parses it into JSON
+    .then(data => console.log(data)) //What im guessing this does: takes the data from before, and turns it into 
+// the var data, and logs it
